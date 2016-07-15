721c480fd28b57130182c7a83c698143f78043af
cornhundred <doctorcorn2002@gmail.com>
2016-07-15 11:29:46 -0400

    adding green highlighting to sidebar

--- load_clustergram.js
+++ load_clustergram.js
@@ -149,6 +149,16 @@ section_fun['reorder_row_alpha'] = function reorder_row_alpha(){
 
 section_fun['reorder_row_var'] = function reorder_row_var(){
   console.log('reorder row variance');
+
+  var highlight_class = 'toggle_row_order';
+  var duration = 4000;
+  d3.select(cgm.params.root+' .'+highlight_class)
+    .style('background','#007f00')
+    .style('box-shadow','0px 0px 10px 5px #007f00')
+    .transition().duration(1).delay(duration)
+    .style('background','#FFFFFF')
+    .style('box-shadow','none');
+
   click_reorder_button('row','rankvar');
 }
 
